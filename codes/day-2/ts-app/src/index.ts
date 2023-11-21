735d6827eb8af053bb8e35eb16bac63e72e429f7
Joydip <joydip.mondal@gmail.com>
2023-11-21 12:27:48 +0530

    created employees.ts file

--- codes/day-2/ts-app/src/index.ts
+++ codes/day-2/ts-app/src/index.ts
@@ -1,6 +1,3 @@
 // import * as Types from "./person";
 // import { Person as PersonInfo } from "./person";
 
-import { Person } from "./person";
-const anilPerson: Person = new Person(1, 'anil', 1000)
-console.log(anilPerson.show())
