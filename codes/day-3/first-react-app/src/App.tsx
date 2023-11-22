cc13172817b843b7c5a5f4d92fa62406811813f7
Joydip <joydip.mondal@gmail.com>
2023-11-22 11:58:25 +0530

    code updated

--- codes/day-3/first-react-app/src/App.tsx
+++ codes/day-3/first-react-app/src/App.tsx
@@ -1,29 +1,4 @@
-//import { createElement } from "react";
-
-/*
-//React Design without JSX/TSX
-
-function App() {
-
-  //data
-  const title = 'Welcome to React JS'
-  const body = 'React JS is a JavaScript library and it helps you create SPA'
-  //design
-  //<div> <-- root element
-  //<h1>Welcome to React JS<h1>
-  //<br>
-  //<p>React JS is a JavaScript library and it helps you create SPA</p>
-  //</div>
-  const titleElement = createElement('h1', null, title)
-  const breakElement = createElement('br')
-  const bodyElement = createElement('p', null, body)
-
-  const divElement = createElement('div', null, [titleElement, breakElement, bodyElement])
-  return divElement
-}
-*/
-//React Design with JSX/TSX
-function App() {
+function App(): JSX.Element {
 
   //data
   const title = 'Welcome to React JS'
@@ -42,4 +17,5 @@ function App() {
   )
   return divElement
 }
+
 export default App
--- codes/day-3/first-react-app/src/index.tsx
+++ codes/day-3/first-react-app/src/index.tsx
@@ -3,21 +3,5 @@ import './index.css';
 import App from './App';
 
 const containerElement = document.getElementById('root') as HTMLElement
-const appDesign = App()
 const root = createRoot(containerElement);
-root.render(appDesign);
-
-//causing a delay
-// setTimeout(
-//     () => {
-//         const containerElement = document.getElementById('root') as HTMLElement
-//         const appDesign = App()
-//         const root = createRoot(containerElement);
-//         root.render(appDesign);
-//     },
-//     5000
-// )
-
-
-
-
+root.render(<App />)
