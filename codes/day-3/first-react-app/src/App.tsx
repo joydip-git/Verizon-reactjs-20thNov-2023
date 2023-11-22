e925a3bf0b23de94fbe259989800b64ae8ca4116
Joydip <joydip.mondal@gmail.com>
2023-11-22 11:45:53 +0530

    App.tsx updated

--- codes/day-3/first-react-app/src/App.tsx
+++ codes/day-3/first-react-app/src/App.tsx
@@ -1,15 +1,45 @@
-import { createElement } from "react";
+//import { createElement } from "react";
+
+/*
+//React Design without JSX/TSX
 
 function App() {
 
   //data
   const title = 'Welcome to React JS'
-
+  const body = 'React JS is a JavaScript library and it helps you create SPA'
   //design
+  //<div> <-- root element
   //<h1>Welcome to React JS<h1>
-  const welcomeElement = createElement('h1', null, title)
+  //<br>
+  //<p>React JS is a JavaScript library and it helps you create SPA</p>
+  //</div>
+  const titleElement = createElement('h1', null, title)
+  const breakElement = createElement('br')
+  const bodyElement = createElement('p', null, body)
 
-  return welcomeElement
+  const divElement = createElement('div', null, [titleElement, breakElement, bodyElement])
+  return divElement
 }
+*/
+//React Design with JSX/TSX
+function App() {
 
+  //data
+  const title = 'Welcome to React JS'
+  const body = 'React JS is a JavaScript library and it helps you create SPA'
+
+  const divElement = (
+    <div>
+      <h1>
+        {title}
+      </h1>
+      <br />
+      <p>
+        {body}
+      </p>
+    </div>
+  )
+  return divElement
+}
 export default App
