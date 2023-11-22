5b2d1c480e434626f6c5c4a6565e2f043fcdbafd
Joydip <joydip.mondal@gmail.com>
2023-11-22 12:20:31 +0530

    Header created

---
+++ codes/day-3/first-react-app/src/components/other/header/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    const content = 'React JS is a JS library, which helps you to create SPA'
+
+    const design = <p>{content}</p>
+
+    return design
+}
+export default Header
