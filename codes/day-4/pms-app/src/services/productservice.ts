d2704aa0fa966f05d68338e3778e9e41e42e4131
Joydip <joydip.mondal@gmail.com>
2023-11-23 11:24:59 +0530

    service updated

--- codes/day-4/pms-app/src/services/productservice.ts
+++ codes/day-4/pms-app/src/services/productservice.ts
@@ -1,15 +1,21 @@
-export const fetchProducts = () => {
+import axiosInstance from "../config/axiosconfig"
+import { ApiResponse } from "../models/apiresponse"
+import { Product } from "../models/product"
 
+const productsUri = 'products'
+export const fetchProducts = () => {
+    return axiosInstance.get<ApiResponse<Product[]>>(productsUri)
 }
 export const fetchProduct = (id: number) => {
-
+    return axiosInstance.get<ApiResponse<Product>>(`${productsUri}/${id}`)
 }
-export const addProduct = (p: any) => {
-
+export const addProduct = (p: Product) => {
+    return axiosInstance.post<ApiResponse<Product[]>>(productsUri, p)
 }
-export const updateProduct = (id: number, p: any) => {
 
+export const updateProduct = (id: number, p: Product) => {
+    return axiosInstance.put<ApiResponse<Product[]>>(`${productsUri}/${id}`, p)
 }
 export const deleteProduct = (id: number) => {
-
+    return axiosInstance.delete<ApiResponse<Product[]>>(`${productsUri}/${id}`)
 }
