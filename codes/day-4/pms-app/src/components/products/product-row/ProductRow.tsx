5684b75c97320d4b1609516d18807ab4035f758b
Joydip <joydip.mondal@gmail.com>
2023-11-23 12:27:52 +0530

    Update ProductRow.tsx

--- codes/day-4/pms-app/src/components/products/product-row/ProductRow.tsx
+++ codes/day-4/pms-app/src/components/products/product-row/ProductRow.tsx
@@ -3,20 +3,23 @@ import { Product } from "../../../models/product"
 type ProductRowPropType = {
     product: Product
 }
+//Pure Function (Component)
 const ProductRow = (props: Readonly<ProductRowPropType>) => {
 
     const { product } = props
-    const design = (<tr>
-        <td>
-            <img src={product.imageUrl} alt="NA" />
-        </td>
-        <td>{product.productName}</td>
-        <td>{product.price}</td>
-        <td>{product.starRating}</td>
-        <td>
-            <button type="button">Delete</button>
-        </td>
-    </tr>)
+    const design = (
+        <tr>
+            <td>
+                <img src={product.imageUrl} alt="NA" />
+            </td>
+            <td>{product.productName}</td>
+            <td>{product.price}</td>
+            <td>{product.starRating}</td>
+            <td>
+                <button type="button">Delete</button>
+            </td>
+        </tr>
+    )
     return design
 }
 
