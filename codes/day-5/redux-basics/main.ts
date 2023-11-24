6636a70f98f3a31529ca059c75265e0ced860121
Joydip <joydip.mondal@gmail.com>
2023-11-24 12:09:39 +0530

    reduxjs/toolkit update

--- codes/day-5/redux-basics/main.ts
+++ codes/day-5/redux-basics/main.ts
@@ -1,5 +1,65 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
 
-// const store = configureStore({
-     
-//  })
+interface CounterStateType {
+    counterValue: number
+}
+interface NameStateType {
+    nameValue: string
+}
+
+const initialCounterState: CounterStateType = {
+    counterValue: 0
+}
+const initialNameState: NameStateType = {
+    nameValue: ''
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialCounterState,
+    reducers: {
+        increaseCounter: (state, action: PayloadAction<number>) => {
+            state.counterValue += action.payload
+        },
+        decreaseCounter: (state, action: PayloadAction<number>) => {
+            state.counterValue -= action.payload
+        }
+    }
+})
+
+const nameSlice = createSlice({
+    name: 'name',
+    initialState: initialNameState,
+    reducers: {
+        updateName: (state, action: PayloadAction<string>) => {
+            state.nameValue = action.payload
+        }
+    }
+})
+
+const counterReducer = counterSlice.reducer
+const nameReducer = nameSlice.reducer
+
+// 'counter/increaseCounter' and
+// 'counter/decreaseCounter'
+// increaseCounter, decreaseCounter
+//increaseCounter(2)=> { type: 'counter/increase' , payload:2}
+
+const { increaseCounter, decreaseCounter } = counterSlice.actions
+
+// 'name/updateName'
+const { updateName } = nameSlice.actions
+
+const loggerMiddleware = createLogger()
+const store = configureStore({
+    reducer: {
+        nameState: nameReducer,
+        counterState: counterReducer
+    },
+    middleware: [loggerMiddleware]
+})
+
+store.dispatch(increaseCounter(2))
+store.dispatch(decreaseCounter(1))
+store.dispatch(updateName('joydip'))
